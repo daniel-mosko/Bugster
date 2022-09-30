08d90adee1c1d6bddcc9f3101c2b05ca8aac21e2
daniel-mosko <59666997+daniel-mosko@users.noreply.github.com>
2022-09-30 10:25:45 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module sk.upjs.bugster {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                        requires org.kordamp.bootstrapfx.core;
+            
+    opens sk.upjs.bugster to javafx.fxml;
+    exports sk.upjs.bugster;
+}
